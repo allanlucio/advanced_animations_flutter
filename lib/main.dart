6b7f9189f37faba875bbe2a4269151f89eebe772
allan <luciocorreia.allan@gmail.com>
2020-04-27 11:16:06 -0300

    Login animated page starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:advanced_animations/animations/grow_transition.dart';
-import 'package:advanced_animations/animations/grow_transparency_transition.dart';
+import 'package:advanced_animations/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,83 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Animations',
       debugShowCheckedModeBanner: false,
-      home: LogoApp(),
+      home: LoginScreen(),
     );
   }
 }
 
-class LogoApp extends StatefulWidget {
-  @override
-  _LogoAppState createState() => _LogoAppState();
-}
 
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  Animation<double> animation;
-  Animation<double> animation2;
-
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 2));
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeInOutQuart);
-
-    animation2 = Tween<double>(begin: 0, end: 150).animate(controller);
-    animation.addStatusListener((status){
-      if(status==AnimationStatus.completed){
-        controller.reverse();
-      }else if(status==AnimationStatus.dismissed){
-        controller.forward();
-      }
-    });
-
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        GrowTransition(
-          animation: animation,
-          child: LogoWidget(),
-        ),
-        GrowTransparencyTransition(
-          animation: animation,
-          child: LogoWidget(),
-        )
-      ],
-    );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-}
-
-class AnimatedLogo extends AnimatedWidget {
-  AnimatedLogo(Animation<double> animation) : super(listenable: animation);
-  @override
-  Widget build(BuildContext context) {
-    final Animation<double> animation = listenable;
-    return Center(
-      child: Container(
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
-    );
-  }
-}
-
-class LogoWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: FlutterLogo(),
-    );
-  }
-}
---
+++ lib/screens/login/widgets/form_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
